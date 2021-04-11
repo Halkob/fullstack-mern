7db0a1d6bd5e75ff3c127523c5c3f006edea53e3
Halcode <32126826+Halkob@users.noreply.github.com>
2021-04-11 22:32:26 +0300

    changed greeting

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({hello: 'there'});
+    res.send({hello: 'there', general: 'kenobi'});
 });
 
 const PORT = process.env.PORT || 5000;
