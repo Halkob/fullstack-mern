7750a984c47ca615d0ccd4cb6ebb292fd8660ac9
Halcode <32126826+Halkob@users.noreply.github.com>
2021-04-24 16:41:29 +0300

    mongodb

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,6 +1,13 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys');
+const mongoose = require('mongoose');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done)=> {
+    done(null, user.id);
+});
 
 passport.use(
     new GoogleStrategy({
@@ -8,9 +15,17 @@ passport.use(
         clientSecret: keys.googleClientSecret,
         callbackURL: '/auth/google/callback'
     }, (accessToken, refreshToken, profile, done) => {
-        console.log('access token:', accessToken);
-        console.log('refresh token:', refreshToken);
-        console.log(profile);
-        
+        User.findOne({googleID: profile.id})
+            .then((existingUser) => {
+                if (existingUser){
+                    //käyttäjä löytyy
+                  done(null, existingUser); 
+                } else{
+                    //tallennetaan uusi käyttäjä tietokantaan
+                    new User({ googleID: profile.id })
+                    .save()
+                    .then(user => done(null, user));
+                }
+            });  
     })
 );
